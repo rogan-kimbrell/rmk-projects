5846ce2faf4563009f63652fe7037652b42974af
rogan-kimbrell <rogank@proton.me>
2026-01-29 13:32:34 -0600

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-
-public class Main{
-   public static void main(String []args){
-   InputStream in = new FileInputStream("../words.txt");
-   Doublets doublets = new Doublets(in);
-   }
-}
